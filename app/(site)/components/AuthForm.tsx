9c7c28e92e80b3b38d64c53cb6ac1256d93d3ec5
hongkbb <481640364@qq.com>
2023-06-09 23:32:58 +0800

    ✨ feat: Register Functionality

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,7 @@ import { BsGithub, BsGoogle } from 'react-icons/bs'
 import Input from '@/app/components/inputs/Input'
 import Button from '@/app/components/Button'
 import AuthSocialButton from './AuthSocialButton'
+import axios from 'axios'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -39,6 +40,7 @@ const AuthForm = () => {
 
     if (variant === 'REGISTER') {
       // TODO: Axios register
+      axios.post('/api/register', data)
     }
 
     if (variant === 'LOGIN') {
